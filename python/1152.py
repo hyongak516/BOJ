a7928dd0b21d5a18756ada546c73c7fb4476e864
hyongak516 <hyongak516@mail.hongik.ac.kr>
2022-04-04 13:56:26 +0900

    .

---
+++ python/1152.py
@@ -0,0 +1,12 @@
+'''
+Copyright (c) 2022 by Hosung.Kim <hyongak516@mail.hongik.ac.kr>
+=====================
+2021.07.09
+Hosung.Kim
+---------------------
+BOJ 1152
+https://www.acmicpc.net/problem/1152
+=====================
+'''
+
+print(len(input().split()))
